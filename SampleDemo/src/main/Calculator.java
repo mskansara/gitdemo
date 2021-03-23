cc0909739961193a8d764a9ccb00d6d6cfafffaf
Manthan Kansara <manthankansara7@gmail.com>
2021-03-23 10:25:01 +0530

    Added sub method in br1

--- SampleDemo/src/main/Calculator.java
+++ SampleDemo/src/main/Calculator.java
@@ -14,5 +14,9 @@ public class Calculator {
 	public static void cuboid(int l ,int b, int h) {
 		System.out.println(l*b*h);
 	}
+	
+	public static void sub(int a, int b) {
+		System.out.println(a-b);
+	}
 
 }
